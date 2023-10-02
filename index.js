7aa0b53421d849e44651add95fd917caa11d8bba
Ibrahimova11 <tu7uncctx@code.edu.az>
2023-10-03 01:22:37 +0400

    commit added

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-let cells = document.querySelectorAll('.cell')
+let cells = document.querySelectorAll('.cell') //.cell sınıfına sahip tüm elementleri seçer ve bu elementleri bir NodeList'ten bir diziye dönüştürür.
 cells = Array.from(cells)
 
-let currentPlayer = "X"
+let currentPlayer = "X" //currentPlayer değişkeni, oyunun şu anki oyuncusunu temsil eder ve "X" ile başlatılır.
 
-let winningCombinations = [
+let winningCombinations = [   //winningCombinations, oyunun kazanan kombinasyonlarını tanımlar. Bu kombinasyonlar, oyunun hangi hücrelerin kazanan bir durumu temsil ettiğini belirlemek için kullanılacak.
     [0,1,2],
     [3,4,5],
     [6,7,8],
@@ -14,22 +14,22 @@ let winningCombinations = [
     [2,4,6]
 ]
 
-function checkForWinner(){
-    winningCombinations.forEach(function(combination){
+function checkForWinner(){  //checkForWinner işlevi, kazananın kontrolünü yapar. 
+    winningCombinations.forEach(function(combination){  //winningCombinations içindeki her kombinasyon için döngüyü dolaşır ve her bir kombinasyonun tüm hücrelerinin şu anki oyuncunun işareti (X veya O) ile doldurulup doldurulmadığını kontrol eder. Bir oyuncu kazandıysa,
         let check = combination.every(idx => cells[idx].innerText.trim() == currentPlayer)
         if(check){
-            highlightCells(combination)
+            highlightCells(combination) //highlightCells işlevini çağırarak kazanan kombinasyondaki hücreleri vurgular.
         }
     })
 }
 
-function highlightCells(combination){
+function highlightCells(combination){ //highlightCells işlevi, kazanan kombinasyondaki hücreleri vurgular. Her bir hücre, classList.add yöntemi kullanılarak "highlight" sınıfını eklenir.
     combination.forEach(function(idx){
         cells[idx].classList.add("highlight")
     })
 }
 
-cells.forEach(function(cell){
+cells.forEach(function(cell){  //cells dizisindeki her bir hücre için bir olay dinleyici ekler. Bu olay dinleyici, bir hücreye tıklama olduğunda çalışır. Önce tıklanan hücrenin boş olup olmadığını kontrol eder. Boşsa, şu anki oyuncunun işaretini (X veya O) ekler, checkForWinner işlemini çağırarak kazananı kontrol eder ve sıradaki oyuncuyu değiştirir. Eğer hücre dolu ise, herhangi bir şey yapmaz ve sırayı değiştirmez.
     cell.addEventListener('click', function(){
         if(cell.innerText.trim() != "") return
         cell.innerText = currentPlayer
